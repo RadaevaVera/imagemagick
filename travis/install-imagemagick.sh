8f609ddee5ef4453b7048099024139129bbfeaa5
RadaevaVera <65042772+RadaevaVera@users.noreply.github.com>
2022-08-08 18:21:55 +0300

    Update install-imagemagick.sh

--- travis/install-imagemagick.sh
+++ travis/install-imagemagick.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 VERSION=7.1.0-45
 set -ex
-wget http://www.imagemagick.org/download/ImageMagick-${VERSION}.tar.gz
-tar -xzvf ImageMagick-${VERSION}.tar.gz
+wget http://www.imagemagick.org/download/ImageMagick-${VERSION}.x86_64.rpm
 cd ImageMagick-${VERSION}  && ./configure --prefix=/usr && make && sudo make install
